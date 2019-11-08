01cbc038b8574229976e8e1b7ec823470dc29e83
Ramona Harrison <ramona.harrison@nytimes.com>
2019-11-07 21:45:20 -0500

    style form

--- src/components/location/Form.js
+++ src/components/location/Form.js
@@ -20,17 +20,39 @@ class Form extends Component {
     }));
   };
 
+  inputStyle = {
+    backgroundColor: '#c06c84',
+    color: 'white',
+    border: 'none',
+    borderRadius: '30px',
+    padding: '12px 20px',
+    margin: '8px 0',
+    boxSizing: 'border-box',
+    fontSize: '20px'
+  }
+
+  buttonStyle = {
+    borderRadius: '50%',
+    backgroundColor: '#c06c84',
+    border: 'none',
+    color: 'white',
+    padding: '20px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    fontSize: '16px',
+    margin: '4px 2px'
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <input
+          style={this.inputStyle}
           onChange={this.handleChange}
           value={this.state.input}
           type="text"
-          placeholder="location"
         />
-
-        <button>+</button>
       </form>
     );
   }
