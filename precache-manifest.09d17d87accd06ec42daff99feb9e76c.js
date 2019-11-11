84956a1e59fa6ae8db96a0192e3ce250c1ac0ba2
Ramona Harrison <ramona.harrison@nytimes.com>
2019-11-11 16:40:46 -0500

    Updates

---
+++ precache-manifest.09d17d87accd06ec42daff99feb9e76c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57e0f92efff1afeca4cd2a87329fc6f",
+    "url": "/surf-clock/index.html"
+  },
+  {
+    "revision": "ee26d3d8d09842bf2670",
+    "url": "/surf-clock/static/css/main.401912af.chunk.css"
+  },
+  {
+    "revision": "745d4c31efe124514e5b",
+    "url": "/surf-clock/static/js/2.04df598c.chunk.js"
+  },
+  {
+    "revision": "ee26d3d8d09842bf2670",
+    "url": "/surf-clock/static/js/main.024f18c5.chunk.js"
+  },
+  {
+    "revision": "6a96433939bfcd8fb083",
+    "url": "/surf-clock/static/js/runtime~main.d9899b18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/surf-clock/static/media/logo.5d5d9eef.svg"
+  }
+]);
